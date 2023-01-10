1dc3adc12448da7652a7b6e227ea2e2f30ad80da
LeLwy <louis.chancioux@gmail.com>
2023-01-10 16:10:26 +0000

    exos canvas

--- exos-canvas/index.js
+++ exos-canvas/index.js
@@ -66,7 +66,6 @@ function exercice4()
             ctx.fill();
             
             circle.x += 10;
-            circle.y -= 5;
             
         }else{
             
@@ -80,6 +79,36 @@ function exercice5()
 {
     let canvasDom = document.getElementById("ex5");
     let  ctx  =  canvasDom.getContext('2d');
+    
+    let circle = {
+    color: "#1c39dd",
+    radius:100,
+    x:canvasDom.width/2,
+    y:canvasDom.height/2,
+    stop: null
+    
+    };
+    
+    circle.stop = setInterval(function(){
+
+        
+        if(circle.radius < canvasDom.height/2){
+            
+            ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+            
+            ctx.fillStyle = circle.color;
+            ctx.beginPath();
+            ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+            ctx.fill();
+            
+            circle.radius += 10;
+            
+        }else{
+            
+            clearInterval(circle.stop);
+        }
+    
+    }, 100);
 
 }
 
@@ -87,7 +116,50 @@ function exercice6()
 {
     let canvasDom = document.getElementById("ex6");
     let  ctx  =  canvasDom.getContext('2d');
+    
+    let pacman = new Image();
+    pacman.src = 'pacman.png';
+    
+    let circle = {
+    color: "#FFD65B",
+    radius:canvasDom.width/4,
+    x:canvasDom.width/2,
+    y:canvasDom.height/2
+    
+    };
 
+    
+    let count = 0;
+    
+    let mouthBite = setInterval(function(){
+        
+        window.addEventListener("click", function(){
+            
+            clearInterval(mouthBite);
+        })
+        
+        if(count%2 === 0){
+            
+            ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+
+            
+                
+            ctx.drawImage(pacman,canvasDom.width/4.25,canvasDom.height/9.7, canvasDom.width/2, canvasDom.width/2);
+        
+            
+        }else{
+            
+            ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+
+            ctx.fillStyle = circle.color;
+            ctx.beginPath();
+            ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+        
+        count ++;
+        
+    }, 1000);
 }
 
 function initCanvasSize()
